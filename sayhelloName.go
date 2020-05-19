11fd796978a9fe18f9e7f4627a783e11ee8ba9e1
GaraYing <984851486@qq.com>
2020-05-19 16:43:27 +0800

    test git revert

--- sayhelloName.go
+++ sayhelloName.go
@@ -12,14 +12,14 @@ func sayHelloName(w http.ResponseWriter, r *http.Request){
 	fmt.Println(r.Form)  //这些信息是输出到服务器端的打印信息
 	fmt.Println("path", r.URL.Path)
 	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
+	fmt.Println(r.Form["url_l"])
 	for k, v := range r.Form {
 		fmt.Println("key:", k)
 		fmt.Println("val:", strings.Join(v, ""))
 	}
 	fmt.Fprintf(w, "Hello astaxie!") //这个写入到w的是输出到客户端的
 }
-
+// test revert
 func main(){
 	http.HandleFunc("/", sayHelloName) // 设置访问路由
 	err := http.ListenAndServe(":9090",nil)
